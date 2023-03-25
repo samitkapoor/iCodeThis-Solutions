24beea9e3f65248456b4a9d7eee316a3ebb22f7d
samitkapoor <samitkapoor77@gmail.com>
2023-03-25 14:54:32 +0530

    agency search 25 march 2023

---
+++ agency search/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#edf4f4",
+      },
+    },
+  },
+  plugins: [],
+};
